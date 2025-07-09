f04a15aad7295a79348d25ba1da287d0e687dba4
Angel Dioses <angel.dioses@unmsm.edu.pe>
2025-07-09 15:27:21 -0500

    CREATE: FeedbackModule.kt - Módulo de inyección de dependencias para feedbacks.

---
+++ app/src/main/java/com/example/sisvitag2/di/FeedbackModule.kt
@@ -0,0 +1,11 @@
+package com.example.sisvitag2.di
+
+import com.example.sisvitag2.data.repository.FeedbackRepository
+import com.example.sisvitag2.ui.vm.FeedbackViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val feedbackModule = module {
+    single { FeedbackRepository(get(), get()) }
+    viewModel { FeedbackViewModel(get()) }
+} 
